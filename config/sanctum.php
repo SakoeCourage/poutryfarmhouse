b182a9e0540dce6dccd7292269c90eb220310f16
General-Work <109393251+General-Work@users.noreply.github.com>
2023-01-31 10:40:27 -0100

    cors check on domain

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,6 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
+        'http://jasotfarm.herokuapp.com',
         'localhost,http://localhost:8000/,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
