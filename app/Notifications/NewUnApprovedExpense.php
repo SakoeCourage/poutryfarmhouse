ef7340ff5128a86853045bcf412285db8426d70e
General-Work <109393251+General-Work@users.noreply.github.com>
2023-02-05 14:55:59 -0100

    ui changes

---
+++ app/Notifications/NewUnApprovedExpense.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\User;
+use App\Models\Expense;
+
+class NewUnApprovedExpense extends Notification
+{
+    use Queueable;
+    public $expense;
+    public $author;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $author,Expense $expense)
+    {
+        $this->expense = $expense;
+        $this->author = $author;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+       
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'author'=>$this->author,
+            'expense' =>$this->expense
+        ];
+    }
+}
